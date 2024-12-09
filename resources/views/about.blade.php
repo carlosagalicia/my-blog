345641fddefd6c2fbca86fed7bfe8a996f2c4e00
CarlosGalicia1 <a01709890@tec.com>
2024-12-09 16:21:36 -0600

    Commit inicial

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app 
+title="About" 
+meta-description="About meta description">
+    <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">Acerca de mi</h1>
+</x-layouts.app>
